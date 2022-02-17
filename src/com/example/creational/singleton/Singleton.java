d649cfd2e67431eb108b8a3589735a0b7ea5d108
GorkemSonmez <gorkemsnmz@hotmail.com>
2022-02-17 08:23:38 +0300

    Second

--- src/com/example/creational/singleton/Singleton.java
+++ src/com/example/creational/singleton/Singleton.java
@@ -3,23 +3,18 @@ package com.example.creational.singleton;
 
 public class Singleton {
 
-    private static Singleton instance ;
+    private static Singleton instance;
 
 
-    private Singleton(){
+    private Singleton() {
         System.out.println("I am creted");
     }
 
     public static Singleton getInstance() {
 
-        if (instance == null){
-            synchronized (Singleton.class){
-                if (instance == null){
-                    instance = new Singleton();
-                }
-            }
+        if (instance == null) {
+            instance = new Singleton();
         }
-
         return instance;
     }
 }
